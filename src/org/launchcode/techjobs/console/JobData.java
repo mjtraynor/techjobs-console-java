b6f5c5438d8e83b869638f5afe16fb4bd655e461
Michael Traynor <traynormichaeljohn@gmail.com>
2019-06-10 23:27:56 +0100

    Final version of assignment, without bonus missions.

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -74,9 +74,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -123,6 +123,24 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+            for (String key : job.keySet()) {
+                if (job.get(key).toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(job);
+                    break;
+                }
+            }
+        }
+
+        return jobs;
     }
 
 }
